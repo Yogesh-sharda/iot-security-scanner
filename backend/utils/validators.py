4aed492524a18de60c9a16fc1c1bb6e917f167bf
Yogesh-sharda <148462311+Yogesh-sharda@users.noreply.github.com>
2026-03-25 04:41:03 +0530

    Update validators.py

--- backend/utils/validators.py
+++ backend/utils/validators.py
@@ -3,7 +3,7 @@ from marshmallow import Schema, fields, validate
 class RegisterSchema(Schema):
     username = fields.String(required=True, validate=validate.Length(min=3, max=30))
     password = fields.String(required=True, validate=validate.Length(min=6))
-    role = fields.String(validate=validate.OneOf(["Admin", "Analyst"]), missing="Analyst")
+    role = fields.String(validate=validate.OneOf(["Admin", "Analyst"]), load_default="Analyst")
 
 class LoginSchema(Schema):
     username = fields.String(required=True)
